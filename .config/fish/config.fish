56b52a209bbb2c800c261655c1286a78fe9ebbe0
VmdJAVem <unixlunatic@proton.me>
2026-01-09 22:18:38 -0500

    fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,18 +3,14 @@
 # Aliases
 alias ls='ls --color=auto'
 alias grep='grep --color=auto'
-alias minecraft='java -jar ~/bin/SKlauncher-3.2.12.jar'
 alias clock='tty-clock -c -s -b -f "%H:%M:%S" -C 3 -B "#6a2c8d" -t'
 alias cowsay='fortune | command cowsay -f stegosaurus'
 alias config='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
 alias cd='z'
 alias vim='nvim'
 alias pdf='zpdf $(fzf)'
-alias gvim="nvim --listen /tmp/godothost"
-alias gayarch="hyfetch --preset bisexual --ascii-file $HOME/neofetch_ascii/arch.txt"
 alias g++="g++ -std=c++23"
 alias cbonsai="cbonsai -l 50 -i -m "
-alias hoi4=" WINEPREFIX=/home/vmdjavem/Games/Heroic/Prefixes/HeartsofIronIV wine '/home/vmdjavem/Games/Heroic/Prefixes/HeartsofIronIV/drive_c/Program Files (x86)/DODI-Repacks/Hearts of Iron IV/dowser.exe'"
 alias xmrig="sudo xmrig -c .config/xmrig.json"
 alias objdump='objdump -M intel'
 alias hd="hexdump -C"
@@ -35,15 +31,13 @@ function fish_prompt
     end
 end
 
-# Homebrew setup
-eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 # fzf key bindings and fuzzy completion
 fzf --fish | source
 
 # PATH configuration
 set -gx PATH $PATH $HOME/.local/bin
 # pdfs
-function spdf
+function zpdf
     for file in $argv
         nohup sioyek "$file" >/dev/null 2>&1 &
     end
@@ -57,9 +51,15 @@ function catfetch
 end
 #zoxide
 zoxide init fish | source
-#fetch
-fastfetch
+
 set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME
 set -gx PATH $HOME/.cabal/bin $PATH /home/vmdjavem/.ghcup/bin # ghcup-env
 #command Prompt
 starship init fish | source
+#fetch
+set fetch $(math $(random 1 100) + 1)
+if test $(math $fetch % 2) = 0
+    fastfetch
+else
+    hyfetch
+end
