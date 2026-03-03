2510e55c11b6fff28ebaed3ecdfd1976677f5575
VmdJAVem <unixlunatic@proton.me>
2026-03-03 14:30:41 -0500

    tmux

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,8 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
 vim.g.godot_executable = "/usr/bin/godot"
 vim.cmd("filetype plugin on")
 vim.cmd("syntax on")
 vim.opt.conceallevel = 2
-vim.g.tpipeline_statusline = "%!v:lua.require('lualine').statusline()"
