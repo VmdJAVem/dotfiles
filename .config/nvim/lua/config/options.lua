72814ea329bfafaa2887cba0e541da1afb2b3936
VmdJAVem <unixlunatic@proton.me>
2026-03-13 21:41:21 -0500

    sd

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,10 +2,11 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.termguicolors = true
-vim.o.tabstop = 8 -- Each TAB character displays as 4 columns
+vim.o.tabstop = 8       -- Each TAB character displays as 4 columns
 vim.o.expandtab = false -- Insert TAB characters instead of spaces
-vim.o.softtabstop = 0 -- Disable space/TAB hybrid behavior
-vim.o.shiftwidth = 8 -- Number of columns for auto-indent and >>/<< operations
+vim.o.softtabstop = 0   -- Disable space/TAB hybrid behavior
+vim.o.shiftwidth = 8    -- Number of columns for auto-indent and >>/<< operations
+vim.opt.wrap = false    -- disable line wrapping globally
 vim.opt.sessionoptions = {
 	"buffers",
 	"curdir",
@@ -26,4 +27,4 @@ vim.api.nvim_create_autocmd("FileType", {
 		vim.opt_local.tabstop = 2 -- how a literal TAB character displays (optional)
 	end,
 })
-vim.opt.clipboard = "unnamedplus"   -- use the system clipboard for yank/paste
+vim.opt.clipboard = "unnamedplus" -- use the system clipboard for yank/paste
