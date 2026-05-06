b8fff24e74746a9f38857ca79ebea75a23eda715
VmdJAVem <unixlunatic@proton.me>
2026-05-05 20:41:26 -0500

    :

--- .config/theme_switcher/themes/gruvbox/nvim/theme.lua
+++ .config/theme_switcher/themes/gruvbox/nvim/theme.lua
@@ -1,38 +1,59 @@
 vim.api.nvim_create_autocmd("ColorScheme", {
 	pattern = "gruvbox",
 	callback = function()
-		vim.api.nvim_set_hl(0, 'DashboardHeader', { link = 'String'
-		})
-		local err = vim.api.nvim_get_hl(0, { name = "ErrorMsg" })
-		vim.api.nvim_set_hl(0, "MiniHipatternsFixme", {
-			italic = true,
-			underline = true,
-			fg = err.fg,
-		})
+		vim.schedule(function()
+			vim.api.nvim_set_hl(0, "DashboardHeader", {
+				link = "String",
+			})
 
-		local yell = vim.api.nvim_get_hl(0, { name = "Question" })
-		vim.api.nvim_set_hl(0, "MiniHipatternsHack", {
-			italic = true,
-			underline = true,
-			fg = yell.fg,
-		})
+			local err = vim.api.nvim_get_hl(0, {
+				name = "ErrorMsg",
+				link = false,
+			})
 
-		local con = vim.api.nvim_get_hl(0, { name = "Include", link = false })
-		vim.api.nvim_set_hl(0, "MiniHipatternsTodo", {
-			italic = true,
-			underline = true,
-			fg = con.fg,
-		})
+			vim.api.nvim_set_hl(0, "MiniHipatternsFixme", {
+				italic = true,
+				underline = true,
+				fg = err.fg,
+			})
 
-		local char = vim.api.nvim_get_hl(0, { name = "Title", link = false })
-		vim.api.nvim_set_hl(0, "MiniHipatternsNote", {
-			italic = true,
-			underline = true,
-			fg = char.fg,
-		})
+			local yell = vim.api.nvim_get_hl(0, {
+				name = "Question",
+				link = false,
+			})
+
+			vim.api.nvim_set_hl(0, "MiniHipatternsHack", {
+				italic = true,
+				underline = true,
+				fg = yell.fg,
+			})
+
+			local con = vim.api.nvim_get_hl(0, {
+				name = "Include",
+				link = false,
+			})
+
+			vim.api.nvim_set_hl(0, "MiniHipatternsTodo", {
+				italic = true,
+				underline = true,
+				fg = con.fg,
+			})
+
+			local char = vim.api.nvim_get_hl(0, {
+				name = "Title",
+				link = false,
+			})
+
+			vim.api.nvim_set_hl(0, "MiniHipatternsNote", {
+				italic = true,
+				underline = true,
+				fg = char.fg,
+			})
+		end)
 	end,
 })
 
+vim.cmd.colorscheme("gruvbox")
 vim.defer_fn(function()
 	vim.cmd.colorscheme("gruvbox")
 end, 100)
