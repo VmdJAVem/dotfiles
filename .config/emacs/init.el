3a32211b75932fb180a7af785ae8bb4a09557c30
VmdJAVem <unixlunatic@proton.me>
2026-05-27 13:41:40 -0500

    emacs

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -10,7 +10,10 @@
  '(custom-safe-themes
    '("27dac7a05a4dabd15ee4fec7c881b172cb8464a11afcf3de6ffad3c61f20247a"
      "9cda0155ffd0cddac60729f5e821ba7955e1623ec7bcb12ca8e7219c33747e0f"
-     default)))
+     default))
+ '(package-vc-selected-packages
+   '((minimal-dashboard :url
+			"https://github.com/dheerajshenoy/minimal-dashboard.el"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
